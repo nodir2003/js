e58c8cd687ec518029c3039cd6247db45a50cb90
nodir2003 <nodirnematov.1234567@gmail.com>
2022-07-19 19:10:32 +0500

    finish

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var ism = prompt('Введите имя');
+console.log('Привет ' + ism);
+var example = +prompt('Введите возраст');
+console.log('Ваш возраст ' + example);
+var example1 = +prompt('Решите пример 6 + 3 = ?');
+console.log('Пример 1: 6 + 3 = 9 Ваш ответ ' + example1);
+var example2 = +prompt('Решите пример 1 - 1 = ?');
+console.log('Пример 2: 1 - 1 = 0 Ваш ответ ' + example2);
+var example3 = +prompt('Решите пример 3 * 3 = ?');
+console.log('Пример 3: 3 * 3 = 9 Ваш ответ ' + example3);
+var example4 = +prompt('Решите пример 8 / 4 = ?');
+console.log('Пример 4: 8 / 4 = 2 Ваш ответ ' + example4);
+var example5 = +prompt('Решите пример 10 % 3 = ?');
+console.log('Пример 5: 10 % 3 = 1 Ваш ответ ' + example5);
+var end = alert('Чтобы посмотреть ответы откройте консоль')
